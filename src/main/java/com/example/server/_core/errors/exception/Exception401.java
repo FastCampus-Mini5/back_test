4064915bb3d425d0758b4e1a3404f5e0c0aa9108
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-07-25 23:40:15 +0900

    feat: 빈번하게 발생하는 Exception 구현 (#2)

---
+++ src/main/java/com/example/server/_core/errors/exception/Exception401.java
@@ -0,0 +1,25 @@
+package com.example.server._core.errors.exception;
+
+
+import com.example.server._core.util.ApiResponse;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+/**
+ * Authentication Failed exception
+ */
+@Getter
+public class Exception401 extends RuntimeException {
+
+    public Exception401(String message) {
+        super(message);
+    }
+
+    public ApiResponse.Result<Object> body() {
+        return ApiResponse.error(getMessage(), status());
+    }
+
+    public HttpStatus status() {
+        return HttpStatus.UNAUTHORIZED;
+    }
+}
