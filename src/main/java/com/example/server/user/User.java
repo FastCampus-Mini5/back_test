13dc52fbcd9a94eb125cd37c139b7fd06c65271e
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-07-25 23:59:12 +0900

    feat: JWT 인증 환경 테스트 용도 임시 유저 서비스 구현 (#2)

---
+++ src/main/java/com/example/server/user/User.java
@@ -0,0 +1,51 @@
+package com.example.server.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+@Table(name = "user_tb")
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 100)
+    private String email;
+
+    @Column(unique = true, nullable = false, length = 100)
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    private String role;
+
+//    private String profile_image;
+
+//    private Timestamp employmentDate;
+
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        role = "user";
+    }
+}
