1282a18a6c327793e5628a46b8736ce6e378b23f
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-07-25 23:46:34 +0900

    feat: 커스텀 UserDetailsService 구현 (#2)

---
+++ src/main/java/com/example/server/_core/security/PrincipalUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.example.server._core.security;
+
+import com.example.server._core.errors.ErrorMessage;
+import com.example.server._core.errors.exception.Exception500;
+import com.example.server.user.User;
+import com.example.server.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class PrincipalUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        log.info("스프링 시큐리티 로그인 서비스 호출 email: " + email);
+
+        Optional<User> userOptional = userRepository.findByEmail(email);
+        User user = userOptional.orElseThrow(() -> new Exception500(ErrorMessage.LOGIN_FAILED));
+
+        return new PrincipalUserDetail(user);
+    }
+}
