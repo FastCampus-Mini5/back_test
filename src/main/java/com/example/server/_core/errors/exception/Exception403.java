90e91cbec436407d59788884b436ffff3e653c45
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-07-25 23:40:15 +0900

    feat: 빈번하게 발생하는 Exception 구현 (#2)

---
+++ src/main/java/com/example/server/_core/errors/exception/Exception403.java
@@ -0,0 +1,25 @@
+package com.example.server._core.errors.exception;
+
+
+import com.example.server._core.util.ApiResponse;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+/**
+ * Authorization failed exception
+ */
+@Getter
+public class Exception403 extends RuntimeException {
+
+    public Exception403(String message) {
+        super(message);
+    }
+
+    public ApiResponse.Result<Object> body() {
+        return ApiResponse.error(getMessage(), status());
+    }
+
+    public HttpStatus status() {
+        return HttpStatus.FORBIDDEN;
+    }
+}
