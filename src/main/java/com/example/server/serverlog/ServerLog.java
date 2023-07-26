701fbcd32c2d86725cc22c6c4dc0bd2e4bb1859e
YongHo Shin <gdtknight@naver.com>
2023-07-26 15:13:11 +0900

    feat: 로그인 로그 엔티티 모델링 (#12)

---
+++ src/main/java/com/example/server/serverlog/ServerLog.java
@@ -0,0 +1,25 @@
+package com.example.server.serverlog;
+
+import com.example.server.user.User;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "log_tb")
+public class ServerLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private User user;
+
+    @Column(nullable = false)
+    private String requestIp;
+
+    @CreatedDate
+    private Timestamp signInDate;
+}
