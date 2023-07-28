341d5b748e61be1b65e09cef7348611b5e1e9f73
YongHo Shin <gdtknight@naver.com>
2023-07-28 14:04:58 +0900

    feat: API 명세서와 일관성 유지를 위해 에러메세지 수정 (#24)

--- src/main/java/com/example/server/_core/errors/ErrorMessage.java
+++ src/main/java/com/example/server/_core/errors/ErrorMessage.java
@@ -13,7 +13,7 @@ public class ErrorMessage {
     public static final String TOKEN_EXPIRED = "만료된 토큰입니다.";
     public static final String TOKEN_VERIFICATION_FAILED = "토큰 검증에 실패했습니다.";
 
-    public static final String EMPTY_DATA_TO_JOIN = "회원 가입을 위한 데이터가 존재하지 않습니다.";
-    public static final String EMPTY_DATA_TO_LOGIN = "로그인을 위한 데이터가 존재하지 않습니다.";
+    public static final String EMPTY_DATA_TO_SIGNUP = "회원 가입을 위한 데이터가 존재하지 않습니다.";
+    public static final String EMPTY_DATA_TO_SIGNIN = "로그인을 위한 데이터가 존재하지 않습니다.";
     public static final String LOGIN_FAILED = "회원 정보가 존재하지 않습니다.";
 }
