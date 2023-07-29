afa94dc57b772738ed9da0ae779b3034f9070578
yangsoohyun <cendy1234@naver.com>
2023-07-30 07:14:56 +0900

    add : 당직 날짜 중복 확인 코드 추가 (#28)

--- src/main/java/com/example/server/schedule/duty/repository/DutyRepository.java
+++ src/main/java/com/example/server/schedule/duty/repository/DutyRepository.java
@@ -2,8 +2,13 @@ package com.example.server.schedule.duty.repository;
 
 
 import com.example.server.schedule.duty.model.Duty;
+import com.example.server.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Timestamp;
+import java.util.Optional;
+
 public interface DutyRepository extends JpaRepository<Duty, Long> {
+    Optional<Duty> findByUserAndDutyDate(User user, Timestamp dutyDate);
 }
 
