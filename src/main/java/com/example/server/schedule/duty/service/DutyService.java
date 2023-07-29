d91b50159d49c2c09da543d9e6505024c3c32001
yangsoohyun <cendy1234@naver.com>
2023-07-30 07:14:56 +0900

    add : 당직 날짜 중복 확인 코드 추가 (#28)

--- src/main/java/com/example/server/schedule/duty/service/DutyService.java
+++ src/main/java/com/example/server/schedule/duty/service/DutyService.java
@@ -1,12 +1,15 @@
 package com.example.server.schedule.duty.service;
 
 
+import com.example.server._core.errors.ErrorMessage;
+import com.example.server._core.errors.exception.Exception400;
 import com.example.server.schedule.duty.model.Duty;
 import com.example.server.schedule.duty.repository.DutyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -15,6 +18,11 @@ public class DutyService {
 
     @Transactional
     public Duty requestDuty(Duty dutyRequest) {
+        Optional<Duty> existingDuty = dutyRepository.findByUserAndDutyDate(dutyRequest.getUser(), dutyRequest.getDutyDate());
+        if (existingDuty.isPresent()) {
+            throw new Exception400(ErrorMessage.DUTY_ALREADY_EXISTS);
+        }
+
         return dutyRepository.save(dutyRequest);
     }
 }
