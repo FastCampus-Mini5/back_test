1b769a6538dddba06da6b92771c91ede66565eb0
yangsoohyun <cendy1234@naver.com>
2023-07-30 20:18:16 +0900

    refactor: 서비스 레이어의 역할 강화와 DTO 이너클래스 사용 리팩토링  (#27)

--- src/main/java/com/example/server/schedule/vacation/dto/VacationRequest.java
+++ src/main/java/com/example/server/schedule/vacation/dto/VacationRequest.java
@@ -3,31 +3,37 @@ package com.example.server.schedule.vacation.dto;
 import com.example.server.schedule.vacation.enums.Reason;
 import com.example.server.schedule.vacation.model.Vacation;
 import com.example.server.user.model.User;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
 
-@Getter
-@Setter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class VacationRequest {
 
-        @NotNull
-        private Reason reason;
+        @Getter
+        @NoArgsConstructor
+        @AllArgsConstructor
+        @Builder
+        @ToString
+        public static class AddVacationDTO {
 
-        @NotNull
-        private Timestamp startDate;
+                @NotNull
+                private Reason reason;
 
-        @NotNull
-        private Timestamp endDate;
+                @NotNull
+                private Timestamp startDate;
 
-        public Vacation toVacationEntity(User user) {
-                return Vacation.builder()
-                        .user(user)
-                        .reason(reason)
-                        .startDate(startDate)
-                        .endDate(endDate)
-                        .build();
+                @NotNull
+                private Timestamp endDate;
+
+                public Vacation toVacationEntity(User user) {
+                        return Vacation.builder()
+                                .user(user)
+                                .reason(reason)
+                                .startDate(startDate)
+                                .endDate(endDate)
+                                .build();
+                }
         }
 }
